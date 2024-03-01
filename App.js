780ce83c0b82067540e408a5fb2ddb257e47af4b
kevinxi@aspendigital.co <kevinxi@aspendigital.co>
2024-03-01 18:11:50 +0800

    updata icon

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image , Platform} from 'react-native';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -13,7 +13,9 @@ import EmojiSticker from './components/EmojiSticker';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
-
+import domtoimage from 'dom-to-image';
+import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen'
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -42,20 +44,40 @@ export default function App() {
   }
 
   const onSaveImageAsync = async() => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved!");
+    if(Platform.OS !== 'web'){
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+  
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    }else{
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current,{
+          quality: 0.95,
+          width:320,
+          height:440
+        });
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e)
+      }
     }
+    
   }
+
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync, 5000);
 
   const pickImageAsync = async() => {
     let result =await ImagePicker.launchImageLibraryAsync({
@@ -97,7 +119,7 @@ export default function App() {
       <EmojiPicker isVisable={isModelVisable} onClose={onModelClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModelClose}></EmojiList>
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
